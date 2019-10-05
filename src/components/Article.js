018313f02eaf9db93fce9ea05fa44fb65234fa14
Anykey Space <20nomad12@mail.ru>
2019-10-05 19:01:44 +0300

    add button, state

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,48 @@
-import React from "react";
+import React, {Component} from "react";
 
-function Article(props) {
-    const {article} = props;
-    const body = <section>{article.text}</section>;
-    return (
-        <div>
-            <h2>{article.title}</h2>
-            {body}
-            <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
-        </div>
-    )
+class Article extends Component {
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = {
+    //         isOpen: false
+    //     };
+    //
+    //     // this.handleClick = handleClick.bind(this)
+    // }
+
+    state = {
+        isOpen: false
+    };
+
+    render() {
+        const {article} = this.props;
+        console.log('---', this.props);
+        const body = this.state.isOpen && <section>{article.text}</section>;
+        return (
+            <div>
+                <h2>
+                    {article.title}
+                    <button onClick={this.handleClick}>
+                        {this.state.isOpen ? 'close' : 'open'}
+                    </button>
+                </h2>
+                {body}
+                <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+            </div>
+        )
+    }
+
+    handleClick = () => {
+        console.log('---', 'clicked');
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
 }
+
+// function handleClick() {
+//     console.log('---', 'clicked')
+// }
 
 export default Article
